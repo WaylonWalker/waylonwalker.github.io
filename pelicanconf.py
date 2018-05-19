6926c2410344c034e61b178f9c1db18e8ccf150a
Waylon S Walker <quadmx08@gmail.com>
2018-05-18 21:21:56 -0500

    setup for duotone layout

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,33 +4,52 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Waylon Walker'
 SITENAME = 'WaylonWalker'
+SITETITLE = 'Waylon Walker'
+SITESUBTITLE = 'Data Scientist'
+SITEDESCRIPTION = 'Blogging about my adventures in data science'
 SITEURL = 'http://www.waylonwalker.com/blog'
+SITEURL = '/blog/'
+MAIN_MENU = True
+
 
 PATH = 'content'
+OUTPUT_PATH = 'blog'
 
 TIMEZONE = 'America/Chicago'
 
 DEFAULT_LANG = 'English'
 
-# THEME = 'pelican-themes\pelican-chunk'
+DISQUS_SITENAME =  'waylonwalker'
+THEME = 'pelican-themes\Flex'
+
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/custom.css'},
+}
+
+CUSTOM_CSS = 'static/custom.css'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = None 
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-# LINKS = (('Home', 'http://getpelican.com/'),
-#          ('Python.org', 'http://python.org/'),
-#          ('Jinja2', 'http://jinja.pocoo.org/'),
-#          ('You can modify those links in your config file', '#'),)
+MENUITEMS = (
+    ('Archives', f'/{OUTPUT_PATH}/archives.html'),
+    ('categories', f'/{OUTPUT_PATH}/categories.html'),
+    ('tags', f'/{OUTPUT_PATH}/tags.html'),
+)
 
+# Blogroll
+LINKS = (
+    ('Home', '/blog/'),
+         ('About', '/'),
+)
 # Social widget
-SOCIAL = (('@_WaylonWalker', 'https://twitter.com/_WaylonWalker'),
-          ('waylonwalker', 'https://github.com/waylonwalker'),
-          ('<i class="fa fa-instagram"></i>@yellowhatwoodworks',
+SOCIAL = (('twitter', 'https://twitter.com/_WaylonWalker'),
+          ('github', 'https://github.com/waylonwalker'),
+          ('instagram',
            'https://www.instagram.com/yellowhatwoodworks/'),
           )
 
